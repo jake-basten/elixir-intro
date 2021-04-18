59776dfb84c9323bb357e27cc0e7c75b8e4ee52c
Jake Basten <jrbas94@gmail.com>
2021-04-18 12:08:46 -0500

    added case for not invalid guesses and will not count invalid guesses towards total guesses

--- src/guess_random_number.ex
+++ src/guess_random_number.ex
@@ -6,6 +6,7 @@ defmodule GuessRandomNumber do
 
   def evaluate_guess(guess, random_number) do
     cond do
+      guess <= 0 or guess > @range -> "invalid guess"
       guess > random_number -> "lower"
       guess < random_number -> "higher"
       true -> "correct"
@@ -17,10 +18,15 @@ defmodule GuessRandomNumber do
                  |> Integer.parse
     evaluation = evaluate_guess(guess, random_number)
 
-    case evaluation == "correct" do
-      false -> IO.puts(evaluation)
-               play_game(random_number, total_guesses + 1)
-      true -> total_guesses
+    cond do
+      evaluation == "correct" ->
+        total_guesses
+      evaluation == "invalid guess" ->
+        IO.puts(evaluation)
+        play_game(random_number, total_guesses)
+      true ->
+        IO.puts(evaluation)
+        play_game(random_number, total_guesses + 1)
     end
   end
 
@@ -31,7 +37,6 @@ defmodule GuessRandomNumber do
     guesses = play_game(random_number)
 
     IO.puts "You guessed the number in #{guesses} guesses! Number was #{random_number}"
-
   end
 
 end
